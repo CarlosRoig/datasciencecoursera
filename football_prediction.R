144612e81ff3b840f6a80165d48d50e68afd9704
Carlos Roig <carlos.roig@godesys.de>
2018-04-03 16:12:30 +0200

    changes

--- football_prediction.R
+++ football_prediction.R
@@ -13,3 +13,26 @@ epl_data <- fdo_listCompFixtures(id = epl_id, response = "minified")$fixtures %>
   rename(home=homeTeamName, away=awayTeamName, homeGoals=result.goalsHomeTeam,
          awayGoals=result.goalsAwayTeam) %>%
   select(home,away,homeGoals,awayGoals)
+poisson_model <-
+  rbind(
+    data.frame(goals=epl_data$homeGoals,
+               team=epl_data$home,
+               opponent=epl_data$away,
+               home=1),
+    data.frame(goals=epl_data$awayGoals,
+               team=epl_data$away,
+               opponent=epl_data$home,
+               home=0)) %>%
+  glm(goals ~ home + team +opponent, family=poisson(link=log),data=.)
+
+simulate_match <- function(foot_model, homeTeam, awayTeam, max_goals=10){
+  home_goals_avg <- predict(foot_model,
+                            data.frame(home=1, team=homeTeam,
+                                       opponent=awayTeam), type="response")
+  away_goals_avg <- predict(foot_model,
+                            data.frame(home=0, team=awayTeam,
+                                       opponent=homeTeam), type="response")
+  dpois(0:max_goals, home_goals_avg) %o% dpois(0:max_goals, away_goals_avg)
+}
+
+simulate_match(poisson_model, "Chelsea", "Sunderland", max_goals=4)
